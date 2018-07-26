974abb43cacd8df50d17708a6f406786bc89bdfc
Mayank Jethva <m.jethva01@gmail.com>
2018-07-25 21:01:22 -0700

    update

--- .docz/app/root.jsx
+++ .docz/app/root.jsx
@@ -10,7 +10,7 @@ const Root = ({ imports }) => (
     config={config}
     entries={entries}
     imports={imports}
-    hashRouter={false}
+    hashRouter={true}
   />
 )
 
---
+++ doczrc.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: 'Mayank Jethva',
+  hashRouter: true,
+}
